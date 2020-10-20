5a85e704eb2654d35e1436e4de19137d65528e30
Saurabh <saurabh.nit20@gmail.com>
2020-10-20 17:47:04 +0530

    Initial Commit

---
+++ MaxUsingGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
